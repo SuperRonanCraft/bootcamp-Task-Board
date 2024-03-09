aa80dee387581e8d77251e0758ed6fc883d58299
RonanCraft <SuperRonanCraft@gmail.com>
2024-03-09 00:15:46 -0500

    colors for task urgency

--- assets/js/board.js
+++ assets/js/board.js
@@ -11,15 +11,16 @@ for (let taskBox of taskBoxEl) {
   taskBox.addEventListener("click", taskClick);
 }
 
+// New Task Button click Event
 function newTask() {
   console.log("New Task Button!");
 }
 
+// Click event for all 3 task containers
 function taskClick(event) {
-  // Check if where clicked in inside a delete button of a task box
-  console.log(event);
+  // Check if where clicked in inside a delete button
   if (event.target.classList.contains("task-delete")) {
-    console.log(event.srcElement);
+    // Remove the task from the page
     event.target.parentElement.remove();
   }
 }
