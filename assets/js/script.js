8534ab9e1cd4f6453d36c4678e018c20e1d2c617
RonanCraft <SuperRonanCraft@gmail.com>
2024-03-12 18:54:27 -0400

    display and create task card

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,10 +13,71 @@ const tasksDone = $("#done-cards");
 function generateTaskId() {}
 
 // Todo: create a function to create a task card
-function createTaskCard(task) {}
+function createTaskCard(task) {
+  const taskContainer = $("<div>");
+
+  //Create Title
+  const taskTitle = $("<h2>");
+  taskTitle.text(task.title);
+  taskContainer.append(taskTitle);
+
+  //Create Description
+  const taskDescription = $("<p>");
+  taskDescription.text(task.description);
+  taskContainer.append(taskDescription);
+
+  //Create Date
+  const taskDate = $("<p>");
+  taskDate.text(task.date);
+  taskContainer.append(taskDate);
+
+  //Create a delete button
+  const taskDelete = $("<button>");
+  taskDelete.text("Delete");
+  //Using Bootstrap css styling
+  taskDelete.addClass("btn-danger").addClass("btn");
+  taskDelete.css("margin-bottom", "5px");
+  taskContainer.append(taskDelete);
+
+  //Add task to todo
+  taskContainer.addClass("task-card");
+
+  //Set how important this is via due date
+  //taskContainer.addClass("task-danger");
+
+  return taskContainer;
+}
 
 // Todo: create a function to render the task list and make cards draggable
-function renderTaskList() {}
+function renderTaskList() {
+  //Remove all previous tasks
+  for (const taskDiv of $(".task-holder")) {
+    $(taskDiv)
+      .children()
+      .each(function () {
+        $(this).remove();
+      });
+  }
+
+  //Load all saved tasks (if they exist...)
+  if (taskList === null) return;
+  for (let task of taskList) {
+    const taskEl = createTaskCard(task);
+
+    //Set the element via its task type
+    switch (task.type) {
+      case "todo":
+        tasksTodo.append(taskEl);
+        break;
+      case "progress":
+        tasksProgress.append(taskEl);
+        break;
+      case "done":
+        tasksDone.append(taskEl);
+        break;
+    }
+  }
+}
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event) {
@@ -27,10 +88,14 @@ function handleAddTask(event) {
     title: inputTitle.val(),
     date: inputDate.val(),
     description: inputDescription.val(),
-    type: "todo",
+    type: "progress",
   };
 
-  createTaskCard(taskData);
+  taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+  taskList.push(taskData);
+  localStorage.setItem("tasks", JSON.stringify(taskList));
+
+  renderTaskList();
 }
 
 // Todo: create a function to handle deleting a task
@@ -41,10 +106,13 @@ function handleDrop(event, ui) {}
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
+  //Render all saved Tasks
+  renderTaskList();
   //Set Datepicker on date picking element on Modal
   $("#formDate").datepicker({
     format: "MM/DD/YYYY",
   });
   //Add click event to forms submit button
   $("#submitTask").click(handleAddTask);
+  $("#tasks").on("click", "btn-danger", function () {});
 });
